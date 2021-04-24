f090c1682c5c18f64b1e3dcabe51b58946fc8969
tony <antbradley91@gmail.com>
2021-04-24 06:57:06 -0400

    add frontend

--- src/Server.hs
+++ src/Server.hs
@@ -6,12 +6,15 @@ module Server
   )
 where
 
-import Control.Concurrent
 import App.DB
+import qualified App.Routes.Predictions as Predictions
+import qualified App.Routes.Stations as Stations
+import Control.Concurrent
 import Data.Aeson
 import qualified Data.ByteString.Char8 as B8
-import qualified Data.ByteString.Lazy.Char8 as LazyB8 
+import qualified Data.ByteString.Lazy.Char8 as LazyB8
 import qualified Data.Text.Lazy as TextLazy
+import Network.HTTP.Types.Header
 import Network.HTTP.Types.Status
 import Network.Wai
 import qualified Network.Wai.Handler.Warp as Warp
@@ -19,8 +22,6 @@ import Network.Wai.Handler.WebSockets
 import qualified Network.Wai.Middleware.Static as Static
 import Network.WebSockets
 import Relude
-import qualified App.Routes.Predictions as Predictions
-import qualified App.Routes.Stations as Stations
 import System.Environment
 import Web.Scotty as Scotty
 
@@ -33,6 +34,11 @@ withEnv action = do
   apiKey <- liftIO getApiKey
   action apiKey
 
+-- | wsApp
+-- Accepts all incoming websocket connections. When
+-- serving them, will retrieve the latest cached
+-- rail predictions from the database and send them
+-- immediately, and then once after every "socketInterval"
 wsApp :: DataBase -> ServerApp
 wsApp db pendingConnection = do
   con <- liftIO $ acceptRequest pendingConnection
@@ -45,11 +51,11 @@ serveConnection db con = do
   liftIO socketInterval
   serveConnection db con
 
-socketInterval :: IO ()
-socketInterval = do
-  threadDelay $ 1000000 * 4
-  return ()
-
+-- | pollPredictions
+-- At an interval specified by "pollInterval", we will
+-- query the WMATA API for the latest predictions and
+-- store them in the Database to be distributed to the
+-- websocket connections
 pollPredictions :: MonadIO m => DataBase -> B8.ByteString -> m ()
 pollPredictions db apiKey = do
   predictions <- Predictions.fetchPredictions apiKey
@@ -62,14 +68,26 @@ pollPredictions db apiKey = do
   where
     encode = Data.Aeson.encode
 
-pollInterval :: IO ()
-pollInterval = do
-  threadDelay $ 1000000 * 15
-  return ()
+-- | spaMiddleware
+-- Simple middleware function that re-routes the url of any
+-- request the accepts "text/html" to point to "index.html".
+-- Enables SPA navigation for the frontend.
+spaMiddleware :: Application -> Application
+spaMiddleware scottyApp req respond =
+  let newReq = req {pathInfo = ["index.html"]}
+      isDocumentRequest =
+        maybe
+          False
+          (B8.isInfixOf "text/html" . snd)
+          (find ((==) hAccept . fst) $ requestHeaders req)
+   in scottyApp (if isDocumentRequest then newReq else req) respond
+  where
+    requestHeaders = Network.Wai.requestHeaders
 
 apiApp :: IO Application
 apiApp = Scotty.scottyApp $ do
-  middleware $ Static.staticPolicy (Static.addBase "client/public") 
+  middleware spaMiddleware
+  middleware $ Static.staticPolicy (Static.addBase "client/dist")
 
   get "/api/stations" $ do
     res <- withEnv Stations.fetchStations
@@ -87,8 +105,6 @@ apiApp = Scotty.scottyApp $ do
   where
     json = Scotty.json
     get = Scotty.get
-    decode :: B8.ByteString -> Either String Predictions.ApiResponse
-    decode = eitherDecode . LazyB8.fromStrict
 
 app :: IO Application
 app = do
@@ -100,3 +116,13 @@ run :: IO ()
 run = do
   getApiKey
   app >>= Warp.run 8000
+
+socketInterval :: IO ()
+socketInterval = do
+  threadDelay $ 1000000 * 4
+  return ()
+
+pollInterval :: IO ()
+pollInterval = do
+  threadDelay $ 1000000 * 15
+  return ()
