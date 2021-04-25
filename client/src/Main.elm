c3dbf9772f281abd0006684d379fd887d1c6c5e3
tony <antbradley91@gmail.com>
2021-04-25 15:22:54 -0400

    begin work on client styling

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -2,9 +2,11 @@ port module Main exposing (..)
 
 import Browser exposing (element)
 import Element as El
-import Element.Background as Bg
-import Element.Border as Bd
-import Element.Events as Ev
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events as Event
+import Element.Font as Font
+import Element.Input as Input
 import Html as H
 import Http
 import Json.Decode as D
@@ -21,20 +23,32 @@ type alias Flags =
 type Msg
     = ReceivedStations (Result Http.Error D.Value)
     | ReceivedPredictions D.Value
+    | SearchTextChanged String
 
 
 type alias Model =
-    {}
+    { searchText : String
+    }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        SearchTextChanged searchText ->
+            ( { model
+                | searchText = searchText
+              }
+            , Cmd.none
+            )
+
+        _ ->
+            ( model, Cmd.none )
 
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    ( {}
+    ( { searchText = ""
+      }
     , Http.request
         { method = "GET"
         , body = Http.emptyBody
@@ -51,7 +65,44 @@ init flags =
 
 view : Model -> H.Html Msg
 view model =
-    El.layout [] (El.text "Hello There")
+    El.layoutWith
+        { options =
+            [ El.focusStyle
+                { borderColor = Just primary
+                , backgroundColor = Nothing
+                , shadow = Nothing
+                }
+            ]
+        }
+        [ Background.color backgroundColor
+        , Font.color text
+        ]
+        (El.column
+            [ El.width El.fill
+            ]
+            [ El.row
+                [ El.centerX
+                , El.paddingXY 0 16
+                ]
+                [ Input.text
+                    [ Background.color overlay1
+                    , Border.color overlay3
+                    , Border.rounded 99999
+                    ]
+                    { label =
+                        Input.labelBelow
+                            [ El.paddingEach { edges | left = 16, top = 4 }
+
+                            ]
+                        <|
+                            El.text "hi"
+                    , onChange = SearchTextChanged
+                    , placeholder = Nothing
+                    , text = model.searchText
+                    }
+                ]
+            ]
+        )
 
 
 subscriptions : Model -> Sub Msg
@@ -67,3 +118,34 @@ main =
         , view = view
         , subscriptions = subscriptions
         }
+
+
+backgroundColor =
+    El.rgba255 18 18 18 1
+
+
+overlay1 =
+    El.rgba255 255 255 255 0.1
+
+
+overlay2 =
+    El.rgba255 255 255 255 0.2
+
+
+overlay3 =
+    El.rgba255 255 255 255 0.3
+
+
+text =
+    El.rgba 255 255 255 0.8
+
+
+primary =
+    El.rgba 255 165 0 1
+
+edges =
+    { top = 0
+    , right = 0
+    , bottom = 0
+    , left = 0
+    }
--- src/Main.elm
+++
@@ -1,8 +0,0 @@
-module Main exposing (..)
-
-import Platform exposing (Program)
-import Browser exposing (element)
-
-
-main :: Program Flags Model Msg
-main
